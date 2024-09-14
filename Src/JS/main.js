c7feb41d35647f7992b9372aefb560f8aef5afa7
Mazen Sadek <148684354+CREPTONICE@users.noreply.github.com>
2024-09-14 18:13:48 +0300

    Add files via upload

---
+++ Src/JS/main.js
@@ -0,0 +1,14 @@
+
+const toggleButton = document.getElementById('modeToggle');
+        const body = document.body;
+
+        toggleButton.addEventListener('click', function() {
+            body.classList.toggle('dark-mode');
+
+            // Change button text based on the mode
+            if (body.classList.contains('dark-mode')) {
+                toggleButton.textContent = 'Switch to Light Mode';
+            } else {
+                toggleButton.textContent = 'Switch to Dark Mode';
+            }
+        });
